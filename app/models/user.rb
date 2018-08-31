ec06c72bb75bfc75bbddaca66bb1562ab3622fdf
davicorreiajr <davicorreiajr@gmail.com>
2018-08-31 19:35:05 +0300

    Create User model

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint(8)        not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class User < ApplicationRecord
+end
