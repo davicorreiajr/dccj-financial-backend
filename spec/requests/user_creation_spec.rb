ad2a03480c8e8caa3fe7495dabef42c5ea173f96
davicorreiajr <davicorreiajr@gmail.com>
2018-09-08 13:13:33 +0300

    fix creation to use factory methods

--- spec/requests/user_creation_spec.rb
+++ spec/requests/user_creation_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe 'User creation', type: :request do
-  let(:user) { User.new(params) }
-  let(:builder) { instance_double('Builders::User') }
   
   before do
     post users_path, params: params, as: :json
@@ -12,6 +10,8 @@ RSpec.describe 'User creation', type: :request do
     let(:name) { 'Davi Cesar' }
     let(:email) { 'davicorreiajr@gmail.com' }
     let(:params) {{ name: name, email: email }}
+    let(:user) { build(:user, name: name, email: email) }
+    let(:builder) { instance_double('Builders::User') }
 
     it 'returns a JSON with :created status' do
       expect_json_and_status(:created)
