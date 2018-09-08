ad68cb8f13e6dc1f04bb04346a8d05d4e44d2392
davicorreiajr <davicorreiajr@gmail.com>
2018-09-08 18:53:49 +0300

    Transaction creation

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -4,9 +4,27 @@ class TransactionsController < ApplicationController
     paginate json: transactions
   end
 
+  def create
+    transaction = account.transactions.build(transaction_params)
+
+    if transaction.save
+      render json: transaction, status: :created
+    else
+      render json: transaction.errors, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def transactions
-    @transactions = Transaction.all
+    @transactions ||= Transaction.all
+  end
+
+  def account
+    @account ||= Account.find(params[:account_id])
+  end
+
+  def transaction_params
+    params.require(:transaction).permit(:value, :description)
   end
 end
---
+++ spec/requests/transaction_creation_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+RSpec.describe 'Transaction creation', type: :request do
+  let(:json) { JSON.parse(response.body) }  
+
+  context 'when account id exists' do
+    let(:account) { create(:account) }
+
+    before do
+      post account_transactions_path(account.id), params: params, as: :json
+    end
+
+    context 'with valid params' do
+      let(:value) { rand(-10.0..10.0).round(2) }
+      let(:description) { 'bleus' }
+      let(:params) {{ value: value, description: description }}
+
+      it 'returns a JSON with :created status' do
+        expect_json_and_status(:created)
+      end
+
+      it 'creates a Transaction' do
+        expect{
+          post account_transactions_path(account.id), params: params, as: :json
+        }.to change(Transaction, :count).by(1)
+      end
+
+      it 'returns an object with the params sent' do
+        expect(json['value']).to eq(value)
+        expect(json['description']).to eq(description)
+      end
+    end
+
+    context 'with invalid params' do
+      let(:params) {{ description: 'bleus' }}
+
+      it 'returns a JSON with :unprocessable_entity status' do
+        expect_json_and_status(:unprocessable_entity)
+      end
+    end
+  end
+
+  context 'when accound id does not exist' do
+    let(:value) { rand(-10.0..10.0).round(2) }
+    let(:description) { 'bleus' }
+    let(:params) {{ value: value, description: description }}
+
+    it 'throws an error' do
+      expect do
+        post account_transactions_path(17), params: params, as: :json
+      end.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
+end
