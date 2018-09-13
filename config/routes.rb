bf9fe463c30d3b5aef9d8174ac25a2113a76ba5a
davicorreiajr <davicorreiajr@gmail.com>
2018-09-13 21:30:16 +0300

    Temp add PasswordsController

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  devise_for :users
+  devise_for :users, skip: :registrations, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords'
+  }
 
   resources :users, only: [:create, :show, :update]
 
