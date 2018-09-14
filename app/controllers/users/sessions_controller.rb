aac0f403f6eb913a7442a5ad13c4a5726e3f8094
davicorreiajr <davicorreiajr@gmail.com>
2018-09-14 18:35:36 +0300

    Fix Users::SessionsController policies

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,6 +3,19 @@
 class Users::SessionsController < Devise::SessionsController
   respond_to :json
 
+  def create
+    super do |resource|
+      skip_authorization
+    end
+  end
+
+  def destroy
+    super do |resource|
+      skip_authorization
+      skip_policy_scope
+    end
+  end
+
   private
 
   def respond_with(resource, _opts = {})
